9c1ce65d5176431e43dd7ff9e959c08e4cb56bc8
cutlerwater <cutlerwater2@live.com>
2022-01-29 11:25:38 -0500

    making little changes

--- app.js
+++ app.js
@@ -518,3 +518,16 @@ projects.forEach(project => {
 
 	app.appendChild(projectEl);
 });
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
